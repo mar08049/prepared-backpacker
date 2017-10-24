9974df53a6fcd38a453a5d20183959a2b89160e7
Cole Martin <mar08049@gmail.com>
2017-10-24 16:50:26 +0000

    updated views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,20 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  delete 'gear/:id/delete' do
+    @gear = Gear.find_by(id: params[:id])
+    @gear.delete
+    redirect to '/gear'
+  end
+
+  helpers do
+    def current_user
+       User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
+
 end
