a20785a1a551b4dff6c945e6fad71727fbdbfe72
Cole Martin <mar08049@gmail.com>
2017-11-03 20:22:31 +0000

    finished

--- app/controllers/gear_controller.rb
+++ app/controllers/gear_controller.rb
@@ -35,42 +35,7 @@ class GearController < ApplicationController
     end
   end
 
-  get '/gear/:id' do
-    if logged_in?
-      @user = current_user
-      @gear = Gear.find_by(id: params[:id])
-      @user.gears << @gear
+  
 
-      erb :'gear/show'
-    else
-      redirect '/'
-    end
-  end
 
-  get '/gear/:id/edit' do
-    @gear = Gear.find_by(id: params[:id])
-      erb :'gear/edit'
-  end
-
-  patch '/gear/:id' do
-    if logged_in? && !params[:name].empty?
-      @gear = Gear.find_by(id: params[:id])
-      @gear.name = (params[:name])
-      @gear.save
-      redirect "/gear/#{@gear.id}"
-    else
-      redirect "/gear/#{params[:id]}/edit"
-    end
-  end
-
-  delete '/gear/:id/delete' do
-    @gear = Gear.find_by(id: params[:id])
-    @user = current_user
-    if logged_in? && @gear.user == current_user
-      @gear.delete
-      redirect '/gear'
-    else
-      redirect "/gear/#{@gear.id}"
-    end
-  end
 end
